1805105b542590da9550ad10bd05ad38b06ce529
asknm <ask.norheim@gmail.com>
2022-11-20 14:07:21 +0100

    Removed console log

--- react/src/types/Bet.ts
+++ react/src/types/Bet.ts
@@ -10,7 +10,6 @@ export async function ToBetWithBetter(snapshot: QueryDocumentSnapshot<BaseBet>):
     const userDoc = await getDoc(doc(getFirestore(), "users", snapshot.id) as DocumentReference<User>);
     const user = userDoc.data();
     const baseBet = snapshot.data();
-    console.log(baseBet);
     return {
         better: user?.name ?? snapshot.id, 
         selection: baseBet.selection,
